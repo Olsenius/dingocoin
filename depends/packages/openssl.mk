336892a388829004be90ac867e5cfe97263a05d8
Andreas Olsen Gulla <andreas.gulla@scaleaq.com>
2025-12-27 10:19:58 +0100

    fix: add isysroot flag for OpenSSL on macOS
    Fixes 'stdio.h' file not found error when building OpenSSL for ARM64 macOS
    by pointing to the correct SDK path.

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -39,6 +39,7 @@ $(package)_config_opts_aarch64_linux=linux-aarch64
 $(package)_config_opts_mipsel_linux=linux-generic32
 $(package)_config_opts_mips_linux=linux-generic32
 $(package)_config_opts_powerpc_linux=linux-generic32
+$(package)_config_opts_darwin=-isysroot $(shell xcrun --show-sdk-path)
 $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
 $(package)_config_opts_aarch64_darwin=darwin64-arm64-cc
 $(package)_config_opts_x86_64_mingw32=mingw64
