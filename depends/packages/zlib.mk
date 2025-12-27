79d731f93dac864cf1c80e0816c24a43d9471e92
Andreas Olsen Gulla <andreas.gulla@scaleaq.com>
2025-12-27 15:30:27 +0100

    fix: define Z_HAVE_UNISTD_H for zlib on macOS
    Fixes fdopen macro conflict with macOS SDK by telling zlib
    that unistd.h is available, avoiding the NO_FSEEKO code path.

--- depends/packages/zlib.mk
+++ depends/packages/zlib.mk
@@ -11,6 +11,7 @@ $(package)_build_opts+=RANLIB="$($(package)_ranlib)"
 $(package)_build_opts+=AR="$($(package)_ar)"
 $(package)_build_opts_darwin+=AR="$($(package)_libtool)"
 $(package)_build_opts_darwin+=ARFLAGS="-o"
+$(package)_build_opts_darwin+=CFLAGS="$($(package)_cflags) $($(package)_cppflags) -fPIC -DHAVE_UNISTD_H -DZ_HAVE_UNISTD_H"
 endef
 
 define $(package)_config_cmds
