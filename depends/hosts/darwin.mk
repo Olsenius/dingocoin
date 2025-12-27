3b9636e256f7ddf607ba168af7370befd50a018e
Andreas Olsen Gulla <andreas.gulla@scaleaq.com>
2025-12-27 12:07:15 +0100

    fix: use system clang and xcrun for native macOS builds
    Simplify darwin.mk to use the system clang toolchain and xcrun
    to find the SDK path. This enables native builds on macOS without
    requiring the native_cctools cross-compilation toolchain.

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,13 +1,8 @@
 OSX_MIN_VERSION=11.0
-OSX_SDK_VERSION=10.11
-OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
-LD64_VERSION=253.9
+OSX_SDK=$(shell xcrun --show-sdk-path)
 
-clang_prog=$(build_prefix)/bin/clang
-clangxx_prog=$(clang_prog)++
-
-darwin_CC=$(build_prefix)/bin/clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION)
-darwin_CXX=$(clang_prog)++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -mlinker-version=$(LD64_VERSION) -stdlib=libc++
+darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK)
+darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -stdlib=libc++
 
 darwin_CFLAGS=-pipe
 darwin_CXXFLAGS=$(darwin_CFLAGS)
@@ -18,4 +13,4 @@ darwin_release_CXXFLAGS=$(darwin_release_CFLAGS)
 darwin_debug_CFLAGS=-O1
 darwin_debug_CXXFLAGS=$(darwin_debug_CFLAGS)
 
-darwin_native_toolchain=native_cctools
+darwin_native_toolchain=
